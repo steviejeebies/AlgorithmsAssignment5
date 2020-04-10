e177e4aa1b3f22d4375142b013879a20fb42b7fa
steviejeebies <rowes@tcd.ie>
2020-04-10 05:44:06 +0100

    added getMapFromFile() function to read in a Map from a text file.

--- src/CompetitionDijkstra.java
+++ src/CompetitionDijkstra.java
@@ -22,7 +22,7 @@ import java.io.IOException;
 import java.util.*;
 
 public class CompetitionDijkstra {
-    class DirectedEdge {
+    static class DirectedEdge {
         private int from;
         private int to;
         private double weight;
@@ -38,18 +38,39 @@ public class CompetitionDijkstra {
         public double getWeight() { return this.weight; }
     }
 
-    class CityMap {
+    class DirectedEdgeComparator implements Comparator<DirectedEdge>{
+
+        // Overriding compare()method of Comparator
+        // for descending order of cgpa
+        public int compare(DirectedEdge e1, DirectedEdge e2) {
+            if (e1.weight < e2.weight)
+                return 1;
+            else if (e1.weight > e2.weight)
+                return -1;
+            return 0;
+        }
+    }
+
+    static class CityMap {
         LinkedList<DirectedEdge> [] adjacent;
-        int index;
+        int numIntersections;
+        int numStreets;
 
         CityMap(int size) {
             adjacent = (LinkedList<DirectedEdge>[]) new LinkedList [size];
-            index = 0;
+            numIntersections = size;
+            numStreets = 0;
         }
 
-        public void addEdge(int from, int to, double weight) {
-            adjacent[from].add(new DirectedEdge(from, to, weight));
+        public void addEdge(DirectedEdge newEdge) {
+            int from = newEdge.getFrom();
+            if(adjacent[from] == null) adjacent[from] = new LinkedList<DirectedEdge>();
+            adjacent[from].add(newEdge);
+            numStreets++;
         }
+
+        public int getNumIntersections() { return numIntersections; }
+        public int getNumStreets() { return numStreets; }
     }
 
     /**
@@ -57,6 +78,29 @@ public class CompetitionDijkstra {
      * @param sA, sB, sC: speeds for 3 contestants
      */
     CompetitionDijkstra (String filename, int sA, int sB, int sC){
+        PriorityQueue<DirectedEdge> myPQ = new PriorityQueue<DirectedEdge>();
+        int slowestWalkingSpeed = sA;
+        if(sB < slowestWalkingSpeed) slowestWalkingSpeed = sB;
+        if(sC < slowestWalkingSpeed) slowestWalkingSpeed = sC;
+        CityMap ourCityMap = getMapFromFile(filename, myPQ);
+        int numIntersections = ourCityMap.getNumIntersections();
+
+        int longestDistanceBetweenTwoVertices = -1;
+
+        // For every intersections in the map, we need to find the shortest distance
+        // from that intersection to every other intersection. In other words, many-to-many
+        // shortest paths, but while using Djikstra (which is a Single Source algorithm).
+        // As a result, we have to iterate through the vertices/intersections, carrying out
+        // Djikstra's algorithm on each one. We'll find the intersection furthest away from
+        // this one, and if the distance between these two intersections is the largest of
+        // all possible combinations, then we save this distance for later.
+//        for(int i = 0; i < numIntersections; i++)
+//        {
+//            double distTo[] = new double [numIntersections];
+//            DirectedEdge edgeTo[] = new DirectedEdge [numIntersections];
+//
+//
+//        }
 
     }
 
@@ -65,25 +109,42 @@ public class CompetitionDijkstra {
      * @return int: minimum minutes that will pass before the three contestants can meet
      */
     public int timeRequiredforCompetition(){
+        //TODO
 
-        //TO DO
         return -1;
     }
 
-    public static double[] getDoubleArrayFromTextFile(String fileName, int arraySize) {
-        double[] returnArray = new double[arraySize];
-        int i = 0; // array index
+    public static CityMap getMapFromFile(String fileName) {
+        CityMap newCityMap;
         try {
             File file = new File(fileName);    //creates a new file instance
             FileReader ourFileReader = new FileReader(file);   //reads the file
             BufferedReader ourBufferedReader = new BufferedReader(ourFileReader);
-            while (i < arraySize)
-                returnArray[i++] = Double.parseDouble(ourBufferedReader.readLine());
+
+            // get number of vertices, and create a new CityMap object
+            newCityMap = new CityMap(Integer.parseInt(ourBufferedReader.readLine()));
+            int numEdges = Integer.parseInt(ourBufferedReader.readLine());
+            int i = 0;
+            String str;
+            while ((str = ourBufferedReader.readLine()) != null) {
+                String[] arrOfStr = str.split(" "); // split line into three (string) numbers
+                // parse as ints and pass as parameters to create new edges on our city map
+                DirectedEdge newEdge = new DirectedEdge(
+                        Integer.parseInt(arrOfStr[0]),
+                        Integer.parseInt(arrOfStr[1]),
+                        Double.parseDouble(arrOfStr[2]));
+                newCityMap.addEdge(newEdge);
+            }
             ourFileReader.close();    //closes the stream and release the resources
+            return newCityMap;
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return returnArray;
+        return null;
+    }
+
+    public static void main(String [] args) {
+        CityMap ourCityMap = getMapFromFile("input-B.txt");
     }
 
 }
