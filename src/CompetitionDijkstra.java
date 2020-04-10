31c6b8ec15ad56cd17f51951d703f330cec7bf0f
steviejeebies <rowes@tcd.ie>
2020-04-10 19:05:25 +0100

    Added method to CityMap to initialise one iteration of Dijkstra's algorithm, so that we can easily switch from one source node to another

--- src/CompetitionDijkstra.java
+++ src/CompetitionDijkstra.java
@@ -39,28 +39,22 @@ public class CompetitionDijkstra {
     }
 
     static class CityMap {
+        // City map related variables/arrays
         LinkedList<DirectedEdge>[] adjacent;
         int numIntersections;
         int numStreets;
 
+        // Dijkstra related variables/arrays
         int sourceNodeDijkstra;
         double distTo[];
-        DirectedEdge edgeTo[];
+        boolean visited[];
+        // DirectedEdge edgeTo[];
         PriorityQueue<Integer> vertexMinDistPQ;
 
         CityMap(int size) {
             adjacent = (LinkedList<DirectedEdge>[]) new LinkedList [size];
             numIntersections = size;
             numStreets = 0;
-        }
-
-        // This method will be called every time a new source vertex/intersection
-        // is examined with Dijkstra's algorithms.
-        public void initialiseDijkstra(int sourceNode) {
-            sourceNodeDijkstra = sourceNode;
-            distTo = new double[numIntersections];
-            edgeTo = new DirectedEdge[numIntersections];
-            vertexMinDistPQ = new PriorityQueue<Integer>(numIntersections, new VertexComparator());
         }
 
         public void addEdge(DirectedEdge newEdge) {
@@ -70,8 +64,30 @@ public class CompetitionDijkstra {
             numStreets++;
         }
 
+        // This method will be called every time a new source vertex/intersection
+        // is examined with Dijkstra's algorithms.
+        public void initialiseDijkstra(int sourceNode) {
+            sourceNodeDijkstra = sourceNode;
+
+            // restart the minPQ from scratch, no nodes on it yet.
+            vertexMinDistPQ = new PriorityQueue<Integer>(new VertexComparator());
+
+            // initialise all the distances in the distTo[] array as ~infinite,
+            // except for distTo[sourceNode], which will be 0
+            distTo = new double[numIntersections];
+            for (int distIter = 0; distIter < numIntersections; distIter++) {
+                distTo[distIter] = (distIter == sourceNodeDijkstra) ? 0 : Integer.MAX_VALUE;
+                vertexMinDistPQ.add(distIter);
+            }
+
+            // no vertices have been visited yet.
+            visited = new boolean[numIntersections];
+
+            //edgeTo = new DirectedEdge[numIntersections];
+        }
+
         // This comparator is used by our vertexMinDistPQ to get the vertex of
-        // shortest distance from the currently examined vertex. 
+        // shortest distance from the currently examined vertex.
         class VertexComparator implements Comparator<Integer>{
             @Override
             public int compare(Integer v1, Integer v2) {
@@ -87,19 +103,35 @@ public class CompetitionDijkstra {
         public int getNumStreets() { return numStreets; }
     }
 
+    // Variables that are used by timeRequiredforCompetition()
+    int slowestWalkingSpeed;
+    int longestDistanceBetweenTwoVertices;
+    boolean invalidMap;
     /**
-     * @param filename: A filename containing the details of the city road network
-     * @param sA, sB, sC: speeds for 3 contestants
-     */
+      * @param filename: A filename containing the details of the city road network
+      * @param sA, sB, sC: speeds for 3 contestants
+      */
     CompetitionDijkstra (String filename, int sA, int sB, int sC){
-        PriorityQueue<DirectedEdge> myPQ = new PriorityQueue<DirectedEdge>();
-        int slowestWalkingSpeed = sA;
+        // We need to get the slowest walking speed of the three contestants
+        slowestWalkingSpeed = sA;
         if(sB < slowestWalkingSpeed) slowestWalkingSpeed = sB;
         if(sC < slowestWalkingSpeed) slowestWalkingSpeed = sC;
-        CityMap ourCityMap = getMapFromFile(filename);
-        int numIntersections = ourCityMap.getNumIntersections();
 
-        int longestDistanceBetweenTwoVertices = -1;
+        // If the map we are given contains scenarios where the competition is impossible to
+        // complete, this boolean will be used in timeRequiredforCompetition() to make sure
+        // that we return '-1' to say that it is invalid. Until the Dijkstra shortest paths
+        // are created from the text file, then we'll also return -1.
+        invalidMap = true;
+
+        // initialise a city map, based on the contents of the text file
+        CityMap ourCityMap = getMapFromFile(filename);
+
+        longestDistanceBetweenTwoVertices = dijkstraLongestDistance(ourCityMap);
+    }
+
+    public int dijkstraLongestDistance(CityMap ourCityMap)
+    {
+        int numIntersections = ourCityMap.getNumIntersections();
 
         // For every intersections in the map, we need to find the shortest distance
         // from that intersection to every other intersection. In other words, many-to-many
@@ -110,25 +142,31 @@ public class CompetitionDijkstra {
         // all possible combinations, then we save this distance for later.
         for(int i = 0; i < numIntersections; i++)
         {
+            // start a new version of the Dijkstra algorithm from scratch,
+            // using 'i' as the source vertex
+            ourCityMap.initialiseDijkstra(i);
+            int nextVertexToVisit = i;
 
+            // if we have already visited this vertex, then we need to get the
+            // next min-distance vertex on the PQ.
+            while(ourCityMap.visited[nextVertexToVisit])
+            {
+                if(ourCityMap.vertexMinDistPQ.isEmpty()); //TODO
+                nextVertexToVisit = ourCityMap.vertexMinDistPQ.poll();
+            }
 
-            for (int distIter = 0; distIter < numIntersections; distIter++)
-                distTo[distIter] = Integer.MAX_VALUE;
+            ourCityMap.visited[nextVertexToVisit]= true;
 
-            distTo[i] = 0;
-
+            // we need to relax all the edges coming from this vertex
 
         }
-
     }
-
 
     /**
      * @return int: minimum minutes that will pass before the three contestants can meet
      */
     public int timeRequiredforCompetition(){
         //TODO
-
         return -1;
     }
 
